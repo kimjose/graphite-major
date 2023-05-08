5792dcaef9f98682c2813cf7717fd1482faaf6d7
kimjose <kimjose693@gmail.com>
2023-05-08 15:52:05 +0300

    drive file model

---
+++ api/Models/DriveFile.php
@@ -0,0 +1,13 @@
+<?php
+namespace Umb\SystemBackup\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DriveFile extends Model{
+
+    protected $fillable = ['id', 'folder_path_id', 'name', 'web_url', 'download_url', 'size', 'created_date_time'];
+
+    public $timestamps = false;
+
+}
+
