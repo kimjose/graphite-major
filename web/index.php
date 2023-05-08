b114a73e750535ec682d3edd30f9d6ce84498598
kimjose <kimjose693@gmail.com>
2023-05-08 15:19:57 +0300

    started on sharepoint

--- web/index.php
+++ web/index.php
@@ -32,51 +32,60 @@ $facilities = Facility::all(); // TODO filter according to user logged in.
 </head>
 
 <body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-        <div class="container">
-            <div class="col-3">
-                <div class="form-group">
-                    <select name="facility" id="selectFacility" class="form-control">
-                        <option value="" selected hidden> Select Facility</option>
-                    </select>
-                </div>
-            </div>
-            <a class="navbar-brand" href="#">
-                <img src="https://placeholder.pics/svg/150x50/888888/EEE/Logo" alt="..." height="36">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Upload</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#backups">Backups</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            System Administration
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#users">Users</a></li>
-                            <li><a class="dropdown-item" href="#facilities">Facilities</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <div class="col-3">
+            <div class="form-group">
+                <select name="facility" id="selectFacility" class=" form-select">
+                    <option value="" selected hidden> Select Facility</option>
+                    <?php foreach ($facilities as $facility): ?>
+                        <option value="<?php echo $facility->id ?>"><?php echo $facility->name ?></option>
+                    <?php endforeach; ?>
+                </select>
             </div>
         </div>
-    </nav>
-    <!-- nav end -->
-    <section>
-        <?php include_once(__DIR__ . "/upload_file.php") ?>
-    </section>
+        <a class="navbar-brand" href="#">
+            <img src="https://placeholder.pics/svg/150x50/888888/EEE/Logo" alt="..." height="36">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Upload</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#backups">Backups</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        System Administration
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#users">Users</a></li>
+                        <li><a class="dropdown-item" href="#facilities">Facilities</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- nav end -->
+<section>
+    <?php include_once(__DIR__ . "/upload_file.php") ?>
+</section>
+
+<script>
+
+</script>
 </body>
 
 </html>
