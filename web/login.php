a38477e9e5b517ecb0198f6acda2b81283c8dfe3
kimjose <kimjose693@gmail.com>
2023-05-16 17:01:18 +0300

    some login

--- web/login.php
+++ web/login.php
@@ -84,12 +84,47 @@
                     </form>
                 </div>
             </div>
+
         </div>
+    <div class="loader"></div>
     </div>
+
 </main>
 
 <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
+<style>
+        #contentSection {
+            width: 100%;
+        }
+
+
+        .loader {
+            z-index: 5;
+            margin-top: -50px;
+            border: 16px solid #f3f3f3;
+            /* Light grey */
+            border-top: 16px solid #3498db;
+            /* Blue */
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            animation: spin 2s linear infinite;
+            margin-left: calc(50% - 60px);
+            margin-top: 20%;
+            float: left;
+        }
+
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
 <script>
     const divSendOtp = document.getElementById('divSendOtp')
     const divVerifyOtp = document.getElementById('divVerifyOtp')
