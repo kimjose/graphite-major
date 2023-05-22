493bb711f8a19ffa78384d680903550fe988bda9
kimjose <kimjose693@gmail.com>
2023-05-22 11:43:10 +0300

    error handling for mail send

--- api/Controllers/Utils/Utility.php
+++ api/Controllers/Utils/Utility.php
@@ -90,7 +90,7 @@ class Utility
     {
         //Create an instance; passing `true` enables exceptions
         $mail = new PHPMailer(true);
-        $footer = "<hr> <h4>Click <a href='http://psms.mgickenya.org:81/event-management/admin/'>here</a> to open event management application. </h4>";
+        $footer = "<hr> <h4>Click <a href='https://psms.mgickenya.org/system-backup/'>here</a> to open the application. </h4>";
         $body .= $footer;
         try {
             //Server settings
@@ -125,10 +125,11 @@ class Utility
             $mail->Body = $body;
             //            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-            $mail->send();
+            return $mail->send();
 //            echo 'Message has been sent';
         } catch (\Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            return false;
+            // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             self::logError($e->getCode(), $e->getMessage());
         }
     }
