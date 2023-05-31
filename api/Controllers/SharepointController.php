599bef101d1d56732a15fb113f4ac7b774ed70f4
kimjose <kimjose693@gmail.com>
2023-05-31 13:25:26 +0300

    moved credentials to .env

--- api/Controllers/SharepointController.php
+++ api/Controllers/SharepointController.php
@@ -17,10 +17,10 @@ class SharepointController
         $curl = curl_init("https://login.microsoftonline.com/1c17770e-a269-4517-b296-c71e84196454/oauth2/v2.0/token");
 
         $postParameter = array(
-            'grant_type' => 'client_credentials',
-            'client_id' => '391e55cd-5287-4e23-9c8d-4d6917944d12',
-            'client_secret' => 'WwN8Q~4jiHGyXqfmTwYRWz2LE3JNixEQymFN_bpe',
-            'scope' => 'https://graph.microsoft.com/.default'
+            'grant_type' => $_ENV['GRANT_TYPE'],
+            'client_id' => $_ENV['CLIENT_ID'],
+            'client_secret' => $_ENV['CLIENT_SECRET'],
+            'scope' => $_ENV['SCOPE']
         );
         curl_setopt($curl, CURLOPT_POSTFIELDS, $postParameter);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
