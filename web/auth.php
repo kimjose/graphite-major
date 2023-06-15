868afcf791f9434684bc53e3be8066691c89759d
kimjose <kimjose693@gmail.com>
2023-06-15 09:00:30 +0300

    auth minor

--- web/auth.php
+++ web/auth.php
@@ -18,14 +18,14 @@ $len = strlen($url);
 $redirect = substr($url, $pos+strlen('web/'), $len);
 $redirect = str_replace('?', '&', $redirect);
 if (!isset($_SESSION[$_ENV['SESSION_APP_NAME']])) {
-    http_response_code(401);
+    // http_response_code(401);
     Utility::logError(401, "User not authenticated,..");
     header('Location: login?redirect='.$redirect);
 //    die(401);
 }
 $sessionData = $_SESSION[$_ENV['SESSION_APP_NAME']];
 if (!isset($sessionData['expires_at'])) {
-    http_response_code(401);
+    // http_response_code(401);
     Utility::logError(401, "User not authenticated,..");
     header('Location: login?redirect='.$redirect);
 //    die(401);
@@ -33,7 +33,7 @@ if (!isset($sessionData['expires_at'])) {
     if (time() > $sessionData['expires_at']) {
         session_unset();
         session_destroy();
-        http_response_code(401);
+        // http_response_code(401);
         Utility::logError(401, "Session expired");
         header('Location: login?redirect='.$redirect);
 //        die(401);
